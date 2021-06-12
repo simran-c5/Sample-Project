089c01180cab36a56cdd6105f19ab3112990223b
Simran Jain <simranmainajain@gmail.com>
2021-06-12 10:48:17 +0530

    card design

--- public/js/userdetails.js
+++ public/js/userdetails.js
@@ -3,23 +3,53 @@ let userContacts = [];
 const showdetails = (data) => {
   let html = ``
   data.forEach((d) => {
-    html = `${html} <div class="col-md-4 col-12 col-sm-6 cardStyling">
-   <div class="card" style="height:21rem;" id="${d._id}">
-     <div class="card-body">
-     <p>name:${d.fname}</p>
-       <p>lastname:${d.lname}</p>
-       <p>phone:${d.phonenumber} </p>
-       <p>address:${d.address}</p> 
-       
-     </div>
-     <div class="deleteBtnClass">
-       <i class="far fa-trash-alt fa-3x "></i>
-     </div>
-     <div class="editBtnClass">
-       <i class="fas fa-edit fa-3x"></i>  
-     </div>
-   </div>
- </div>
+    html = `${html} <div class="col-md-4 col-12 col-sm-6 cardStyling" id="${d._id}">
+    <div class="card" style="height:21rem;">
+      <div class="card-body">
+        <div class="dropdown" style=" float: right; margin-top: -12px;
+      margin-right: 5px;">
+          <i data-toggle="dropdown" class=" fas fa-ellipsis-v"></i>
+          <div class="dropdown-menu" id="${d._id}">
+            <div class="deleteBtnClass">
+              <a class="dropdown-item "> <i class="far fa-trash-alt fa-lg "> delete</i>
+              </a>
+            </div>
+            <div class="editBtnClass">
+              <a class="dropdown-item"> <i class="fas fa-edit fa-lg"> edit</i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="container mt-3">
+          <div class="profileImage "
+            style="width: 100px; height: 100px; border-radius: 50%; margin: 0 auto; background-color: red;">
+            <img style="width: 100px; height: 100px; border-radius: 50%;"
+              src="https://thumbs.dreamstime.com/b/male-icon-vector-user-person-profile-avatar-flat-color-glyph-pictogram-illustration-117610350.jpg"
+              alt="">
+          </div>
+        </div>
+        <div class="container mt-3" style="text-align: center;">
+          <p style="font-size: 1.7rem;
+      font-weight: bold;
+      font-family: sans-serif; margin-bottom: 0;">${d.fname} ${d.lname}</p>
+        </div>
+        <div class="container" style="text-align: center;">
+          <p style="font-size: 1.3rem;
+      font-weight: bold;
+      font-family: fangsong;
+      color: #babcbb;">${d.address}</p>
+        </div>
+        <div class="container" style="text-align: center;
+      padding: 8% 0%;
+      display: flex;
+      justify-content: space-evenly;">
+          <i class="fab fa-whatsapp  fa-2x"></i>
+          <i class="fas fa-phone-square  fa-2x"></i>
+          <i class="far fa-envelope  fa-2x"></i>
+        </div>
+      </div>
+    </div>
+  </div>
 `
   })
   $("#divParentDetail").html(html);
